b5174d258daa60998adc934f641c9333c458a721
neitgnud <neitgnud@gmail.com>
2022-11-30 09:25:17 +0700

    remove useless

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,17 @@
-var CollectionFactory = artifacts.require("CollectionFactory");
-var Farming = artifacts.require("Farming");
+var Hub = artifacts.require("Hub");
 var Marketplace = artifacts.require("Marketplace");
 var NFTLoan = artifacts.require("NFTLoan");
+var Rental = artifacts.require("Rental");
 var PNFT = artifacts.require("PNFT");
-var TestERC20 = artifacts.require("TestERC20");
+var FungibleToken = artifacts.require("FungibleToken");
 
 module.exports = function (deployer) {
-    deployer.deploy(CollectionFactory, 1000000);
-    deployer.deploy(Farming, 1000);
-    deployer.deploy(Marketplace, 1000);
-    deployer.deploy(NFTLoan, 100);
-    deployer.deploy(PNFT, "Picasarts", "PNFT", "0xD766119a5F4c9Db9823F08034DB770CFe24b4Ac5");
-    deployer.deploy(TestERC20, "Maker DAI", "DAI");
-    deployer.deploy(TestERC20, "Coinbase USD", "USDC");
-    deployer.deploy(TestERC20, "Tether USD", "USDT");
+    deployer.deploy(Hub, 25);
+    deployer.deploy(Rental, 25);
+    deployer.deploy(Marketplace, 25);
+    deployer.deploy(NFTLoan, 25);
+    deployer.deploy(PNFT, "Picasarts", "PNFT", "0xf8805f4FAFd0bef941B9573447005B846DBf1303");
+    deployer.deploy(FungibleToken, "Maker DAI", "DAI");
+    deployer.deploy(FungibleToken, "Coinbase USD", "USDC");
+    deployer.deploy(FungibleToken, "Tether USD", "USDT");
 };
